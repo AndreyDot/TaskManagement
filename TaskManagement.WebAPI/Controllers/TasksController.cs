2d4e9af10efe2a30f16d7670143f06c6628cf83e
Andrii Osetskyi <osetskyyandriy@gmail.com>
2026-04-06 21:33:17 +0300

    [feat] Add TasksController with CreateTask endpoint using MediatR

---
+++ TaskManagement.WebAPI/Controllers/TasksController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Application.Features.Tasks.Create;
+using TaskManagement.Application.Features.Tasks.DTOs;
+
+namespace TaskManagement.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TasksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskDto>> CreateTask([FromBody] CreateTaskDto dto)
+        {
+            CreateTaskCommand command = new CreateTaskCommand(dto);
+
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
