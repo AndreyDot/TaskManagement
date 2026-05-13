5659e0269dab5d29c136e57fa30f9cd1460b20c7
Andrii Osetskyi <osetskyyandriy@gmail.com>
2026-05-13 14:21:39 +0300

    feat: add custom exception handling middleware

---
+++ TaskManagement.WebAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation;
+using System.Net;
+using System.Text.Json;
+using System.Linq;
+
+namespace TaskManagement.WebAPI.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = ex switch
+            {
+                KeyNotFoundException => (int)HttpStatusCode.NotFound,
+                UnauthorizedAccessException => (int)HttpStatusCode.Forbidden,
+                ValidationException => (int)HttpStatusCode.BadRequest,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            var message = ex switch
+            {
+                ValidationException validationEx =>
+                    string.Join(" | ", validationEx.Errors.Select(e => e.ErrorMessage)),
+                _ => ex.Message
+            };
+
+            var response = new
+            {
+                statusCode,
+                message
+            };
+
+
+            context.Response.StatusCode = statusCode;
+
+            context.Response.ContentType = "application/json";
+
+            var json = JsonSerializer.Serialize(response);
+
+            await context.Response.WriteAsync(json);
+        }
+
+    }
+}
